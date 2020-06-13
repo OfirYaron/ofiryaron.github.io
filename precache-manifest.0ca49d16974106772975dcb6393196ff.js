2d7fa289fef63337b9567893072dceb551ca2811
ofiryaron <ofirya@gmail.com>
2020-06-13 22:50:36 +0300

    First commit

---
+++ precache-manifest.0ca49d16974106772975dcb6393196ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477c87f80286621c6467a77b3f836e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32bf740a74d4d5ec19fc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1fe97f21dfc64921a5d1",
+    "url": "/static/js/2.5bcec515.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5bcec515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32bf740a74d4d5ec19fc",
+    "url": "/static/js/main.b8896458.chunk.js"
+  },
+  {
+    "revision": "86d0368fe87bf489db70",
+    "url": "/static/js/runtime-main.08c6b2f5.js"
+  }
+]);
