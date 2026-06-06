31e8c3e6bbf2d837dfda677eaaec811835abbdc4
OfirYaron <OfirYaron@users.noreply.github.com>
2026-06-06 12:43:27 +0000

    Deploying to gh-pages from @ OfirYaron/ofiryaron.com@10bb56d9a263f09d410c9df42f41ac4d8c0b44ee 🚀

--- assets/About-2FCKHKUm.js
+++ assets/About-2FCKHKUm.js
@@ -1,4 +1,4 @@
-import{d as e,g as t,r as n,t as r}from"./jsx-runtime-DAa8HyWv.js";import{r as i}from"./index.module-vyY-s47a.js";import{t as a}from"./index-B1WRPqZX.js";import{n as o,r as s,t as c}from"./lib-B1pj7yl_.js";var l=t(i());e();var u=`# **OFIR YARON**
+import{d as e,g as t,r as n,t as r}from"./jsx-runtime-DAa8HyWv.js";import{r as i}from"./index.module-vyY-s47a.js";import{t as a}from"./index-Cbs-P-k1.js";import{n as o,r as s,t as c}from"./lib-B1pj7yl_.js";var l=t(i());e();var u=`# **OFIR YARON**
 **Cloud Solution Architect | Former CTO | AI Workflow Orchestrator** 
 [ofir@ofiryaron.com](mailto:ofir@ofiryaron.com) | [GitHub](https://github.com/OfirYaron) | [LinkedIn](https://www.linkedin.com/in/ofiryaron)
 
