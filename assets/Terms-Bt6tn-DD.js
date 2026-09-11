399941d712cc46206e90387f73b41cd00a4ea45a
OfirYaron <OfirYaron@users.noreply.github.com>
2026-09-11 10:24:45 +0000

    Deploying to gh-pages from @ OfirYaron/ofiryaron.com@c16cc89bc063b3a39b8186c573443f5f005af980 🚀

--- assets/Terms-Bt6tn-DD.js
+++ assets/Terms-Bt6tn-DD.js
@@ -1,4 +1,4 @@
-import{h as e,t}from"./jsx-runtime-Bxl4I8Me.js";import"./lib-Bh1uRD5L.js";import{t as n}from"./MarkdownPage-BMvouo3Z.js";e();var r=`*Last updated: July 26, 2026*
+import{h as e,t}from"./jsx-runtime-Bxl4I8Me.js";import"./lib-Bh1uRD5L.js";import{t as n}from"./MarkdownPage-JR5NV2iz.js";e();var r=`*Last updated: July 26, 2026*
 
 ## 1. Acceptance of Terms
 
