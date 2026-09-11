42217ad843eb16ff86eb7fb9e2c6c2e2a5466558
OfirYaron <OfirYaron@users.noreply.github.com>
2026-09-11 09:21:59 +0000

    Deploying to gh-pages from @ OfirYaron/ofiryaron.com@517a6c4bbc4f7f5e8f5e6c61485ec711c4d3166c 🚀

--- assets/Privacy-B2dcvKXG.js
+++ assets/Privacy-B2dcvKXG.js
@@ -1,4 +1,4 @@
-import{h as e,t}from"./jsx-runtime-Bxl4I8Me.js";import"./lib-Bh1uRD5L.js";import{t as n}from"./MarkdownPage-CuLErdLC.js";e();var r=`*Last updated: July 26, 2026*
+import{h as e,t}from"./jsx-runtime-Bxl4I8Me.js";import"./lib-Bh1uRD5L.js";import{t as n}from"./MarkdownPage-Dm-SAKUR.js";e();var r=`*Last updated: July 26, 2026*
 
 ## 1. What Data We Collect
 
